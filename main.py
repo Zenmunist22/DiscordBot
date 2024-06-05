4988b2cab0ac8aa456bf4bdaccadf1994af32a75
Tommy C <Tommychang22@Hotmail.com>
2024-06-05 14:11:26 -0400

    init.

---
+++ main.py
@@ -0,0 +1,24 @@
+# This example requires the 'message_content' intent.
+
+import discord
+
+discordSecretToken=""
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hey'):
+        await message.channel.send('Hello!')
+
+client.run(discordSecretToken)
