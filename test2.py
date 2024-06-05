a31f650985da539fdf34c0f57140eb56b45b15ef
okno1234 <eduybombom@gmail.com>
2024-06-05 19:11:49 -0400

    Menu Blueprint
    Basic menu options

---
+++ test2.py
@@ -0,0 +1,12 @@
+def option_2(num: str):
+    match num:
+        case "1":
+            print("Here's Tommy's balance: \n")
+        case "2":
+            print("Here's Hiram's balance: \n")
+        case "3":
+            print("Here's Hazel's balance: \n")
+        case "4":
+            print("Here's Kyle's balance: \n")
+        case "5":
+            print("Here's Michael's balance: \n")
