ce3491e78183216bae705b7291168d8debc29dbe
okno1234 <eduybombom@gmail.com>
2024-06-09 18:48:56 -0400

    Transactions class

---
+++ transactions.py
@@ -0,0 +1,7 @@
+class Transactions:
+    def __init__(self, amount: float, charged: str) -> None:
+        self.amount = amount
+        self.charged = charged
+        #Generate ID maybe
+        
+    
