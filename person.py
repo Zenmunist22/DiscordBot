f44cd065cebb2e894e9212f0559cba50b2dbcce4
okno1234 <eduybombom@gmail.com>
2024-06-13 19:53:15 -0400

    Person and Transaction init

--- person.py
+++ person.py
@@ -1,5 +1,10 @@
 class Person:
-    def __init__(self, phone: str, email: str) -> None:
-        #Generate ID
+    def __init__(self, ID: None, name: str, phone: str, email: str, created_at: str, modified_at: str) -> None:
+        self.name = name
         self.phone = phone
         self.email = email
+        self.created_at = created_at
+        self.modified_at = modified_at
+        self.ID = ID
+        
+    
--- transactions.py
+++ transactions.py
@@ -1,7 +1,17 @@
 class Transactions:
-    def __init__(self, amount: float, charged: str) -> None:
+    def __init__(self, ID, title, category, amount, charged_to, charged_from, description, created_by, created_at, modified_by, modified_at, due_date) -> None:
+        self.ID = ID
+        self.title = title
+        self.title = title
         self.amount = amount
-        self.charged = charged
-        #Generate ID maybe
+        self.charged_to = charged_to
+        self.charged_from = charged_from
+        self.description = description
+        self.created_by = created_by
+        self.created_at = created_at
+        self.modified_by = modified_by
+        self.modified_at = modified_at
+        self.due_date = due_date
+
         
     
