744d5d86302858c13515582917de02fa5503e531
Tommy Chang <tommychang22@hotmail.com>
2024-08-14 01:06:56 -0400

    init

---
+++ vite-mantine/src/app/main.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import * as ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import {
+  createBrowserRouter,
+  Router,
+  RouterProvider,
+} from "react-router-dom";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <RouterProvider router = {router}>
+      
+    </RouterProvider>
+  </React.StrictMode>
+);
